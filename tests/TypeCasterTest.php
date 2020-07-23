1a5a59b78eb0218ccd0e7ae2f124486b1eb36f78
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-07-23 16:55:09 +0300

    Minor refactoring.

--- tests/TypeCasterTest.php
+++ tests/TypeCasterTest.php
@@ -15,41 +15,6 @@ class TypeCasterTest extends TestCase
         $this->caster = new TypeCaster();
     }
 
-    public function testToIntOrNull()
-    {
-        $this->assertNull($this->caster->toIntOrNull(null));
-        $this->assertNull($this->caster->toIntOrNull(''));
-        $this->assertSame($this->caster->toIntOrNull(1), 1);
-        $this->assertSame($this->caster->toIntOrNull('1'), 1);
-        $this->assertSame($this->caster->toIntOrNull('5 200'), 5200);
-    }
-
-    public function testToStringOrNull()
-    {
-        $this->assertNull($this->caster->toStringOrNull(null));
-        $this->assertNull($this->caster->toStringOrNull(''));
-        $this->assertSame($this->caster->toStringOrNull('1'), '1');
-        $this->assertSame($this->caster->toStringOrNull(2), '2');
-    }
-
-    public function testToFloat()
-    {
-        $this->assertSame($this->caster->toFloat(null), 0.0);
-        $this->assertSame($this->caster->toFloat(''), 0.0);
-        $this->assertSame($this->caster->toFloat(1.25), 1.25);
-        $this->assertSame($this->caster->toFloat('1.25'), 1.25);
-        $this->assertSame($this->caster->toFloat('1 542,21'), 1542.21);
-    }
-
-    public function testToFloatOrNull()
-    {
-        $this->assertNull($this->caster->toFloatOrNull(null));
-        $this->assertNull($this->caster->toFloatOrNull(''));
-        $this->assertSame($this->caster->toFloatOrNull(1.25), 1.25);
-        $this->assertSame($this->caster->toFloatOrNull('1.25'), 1.25);
-        $this->assertSame($this->caster->toFloatOrNull('1 542,21'), 1542.21);
-    }
-
     public function testToBool()
     {
         $this->assertTrue($this->caster->toBool(true));
@@ -64,5 +29,40 @@ class TypeCasterTest extends TestCase
         $this->assertFalse($this->caster->toBool('no'));
         $this->assertFalse($this->caster->toBool('false'));
         $this->assertFalse($this->caster->toBool(null));
+    }
+
+    public function testToFloat()
+    {
+        $this->assertSame($this->caster->toFloat(null), 0.0);
+        $this->assertSame($this->caster->toFloat(''), 0.0);
+        $this->assertSame($this->caster->toFloat(1.25), 1.25);
+        $this->assertSame($this->caster->toFloat('1.25'), 1.25);
+        $this->assertSame($this->caster->toFloat('1 542,21'), 1542.21);
+    }
+
+    public function testToIntOrNull()
+    {
+        $this->assertNull($this->caster->toIntOrNull(null));
+        $this->assertNull($this->caster->toIntOrNull(''));
+        $this->assertSame($this->caster->toIntOrNull(1), 1);
+        $this->assertSame($this->caster->toIntOrNull('1'), 1);
+        $this->assertSame($this->caster->toIntOrNull('5 200'), 5200);
+    }
+
+    public function testToFloatOrNull()
+    {
+        $this->assertNull($this->caster->toFloatOrNull(null));
+        $this->assertNull($this->caster->toFloatOrNull(''));
+        $this->assertSame($this->caster->toFloatOrNull(1.25), 1.25);
+        $this->assertSame($this->caster->toFloatOrNull('1.25'), 1.25);
+        $this->assertSame($this->caster->toFloatOrNull('1 542,21'), 1542.21);
+    }
+
+    public function testToStringOrNull()
+    {
+        $this->assertNull($this->caster->toStringOrNull(null));
+        $this->assertNull($this->caster->toStringOrNull(''));
+        $this->assertSame($this->caster->toStringOrNull('1'), '1');
+        $this->assertSame($this->caster->toStringOrNull(2), '2');
     }
 }
